9e97ac553f1c236535bb07f7d5574b112eacaaab
Erik Agayan <erik.agayan1@gmail.com>
2023-09-25 22:49:46 +0300

    Add schemas

---
+++ schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: float
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductUpdate(ProductBase):
+    pass
+
+
+class Product(ProductBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
