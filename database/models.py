5652ab616e2db41b62a2f7a7d2e3e952bd1c765b
Erik Agayan <erik.agayan1@gmail.com>
2023-09-25 22:42:40 +0300

    Add database folder, engine and models

---
+++ database/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from database.engine import Base
+from sqlalchemy import Column, Integer, String, DateTime, Float
+
+
+class DBProduct(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    description = Column(String(511), nullable=False)
+    price = Column(Float)
+    created_at = Column(DateTime, default=datetime.utcnow)
