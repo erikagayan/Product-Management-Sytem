731a0fc2b28927c8a3dcfabd1cca9453dcb70ee5
Erik Agayan <erik.agayan1@gmail.com>
2023-09-26 21:58:50 +0300

    Add create_product endpoint

--- crud.py
+++ crud.py
@@ -7,3 +7,11 @@ from schemas import ProductCreate, ProductUpdate
 
 def get_all_products(database: Session, product_id: int):
     return database.query(DBProduct).filter(DBProduct.id == product_id).first()
+
+
+def create_product(database: Session, product: ProductCreate):
+    database_product = DBProduct(**product.model_dump())
+    database.add(database_product)
+    database.commit()
+    database.refresh(database_product)
+    return database_product
