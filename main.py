b0c036b7805b724f6ff0a0536cedbae0fdcaab77
Erik Agayan <erik.agayan1@gmail.com>
2023-09-26 22:09:26 +0300

    Add update_product endpoint

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import crud
 import schemas
 from dependencies import get_db
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
-from routers import get_product_by_id, create_product
+from fastapi import FastAPI, Depends, HTTPException
+from routers import get_product_by_id, create_product, update_product
 
 
 app = FastAPI()
 
 app.include_router(get_product_by_id.router)
 app.include_router(create_product.router)
+app.include_router(update_product.router)
